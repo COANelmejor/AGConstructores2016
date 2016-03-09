3ec4a5f13b59064fe7054e6aa39c9e3da558b96b
COANelmejor <coanelmejor@gmail.com>
2016-03-08 18:38:04 -0600

    Termiando la primera version de proyectos.html | No se agregaron las imagenes al commit porque son demasiado pesadas

---
+++ proyectosjQuery.js
@@ -0,0 +1,463 @@
+var agc = [
+	{
+		"name": "Café Sunzal",
+		"ubicacion": "Km 42 de la carretera del Litoral, La Libertad, El Salvador.",
+		"area": "400 m²",
+		"arquitectura": "Arq. Ricardo González Zeceña",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Alejandro González",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "02",
+		"imagesFiles": [
+			"1.jpg",
+			"2.jpg",
+			"3.jpg",
+			"4.jpg",
+			"5.jpg",
+			"6.jpg",
+			"7.jpg"
+		],
+		"descripcion": "El proyecto está ubicado en un área turística sobre  la carretera Litoral de la costa sur de el Salvador, frente al Océano Pacifico.<br> La idea principal de su arquitectura es brindar a los usuarios un ambiente fresco,aprovechando la briza marina y las corrientes naturales de viento y a su vez impactar con la imponente vista hacia el mar."
+	},
+	{
+		"name": "Casa De Botes De Remo",
+		"ubicacion": "Club Salvadoreño, Corinto, Lago de Ilopango, San Salvador, El Salvador.",
+		"area": "250.00 m²",
+		"arquitectura": "Arq. Ricardo González Zeceña",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Mario Reni Roldan",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "03",
+		"imagesFiles": [
+			"1.jpg",
+			"2.jpg",
+			"3.jpg",
+			"4.jpg",
+			"5.jpg",
+			"6.jpg",
+			"7.jpg"
+		],
+		"descripcion": "La casa de botes se ubica a la orilla del lago de Ilopango, su localización al borde de la playa facilita a los atletas la colocación del equipo hacia el agua. Como parte de la creatividad empleada en este proyecto se utilizó la estructura de rompe olas como graderío para el público."
+	},{
+		"name": "Casa Imberton",
+		"ubicacion": "Bosques de Santa Elena, Antiguo Cuscatlán San Salvador, El Salvador",
+		"area": "470.00 m²",
+		"arquitectura": "Arq. Rolando Deneke Sol<br />Arq. Alejandro González Zeceña<br/>Arq. Ricardo González Zeceña",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Alejandro González",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "06",
+		"imagesFiles": [
+			"1.jpg",
+			"2.jpg",
+			"3.jpg",
+			"4.jpg",
+			"5.jpg"
+		],
+		"descripcion": "Su arquitectura moderna de tipo minimalista refleja el espíritu artístico de su propietario, buscando la belleza en la simplicidad y forma pura de los elementos y materiales utilizados para su  construcción."
+	},
+	{
+		"name": "Bosque Nebulo",
+		"ubicacion": "Volcán de Santa Ana, El Salvador",
+		"area": "180 m²",
+		"arquitectura": "Arq. Ricardo González Zeceña",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Alejandro González",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "01",
+		"imagesFiles": [
+			"1.jpg",
+			"2.jpg",
+			"3.jpg",
+			"4.jpg",
+			"5.jpg",
+			"6.jpg"
+		],
+		"descripcion": "Este proyecto se  ubicá en el bosque  Nebuloso  del volcán de Santa Ana, su diseño intenta incorporarse a las curvas de nivel del terreno para poder tener el menor impacto visual posible con el entorno."
+	},
+	{
+		"name": "Casa Club La Montaña",
+		"ubicacion": "Volcán de Santa Ana, Santa Ana, El Salvador",
+		"area": "350.00 m²",
+		"arquitectura": "Arq. Ricardo González Zeceña",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Alejandro González",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "04",
+		"imagesFiles": [
+			"1.jpg",
+			"2.jpg",
+			"3.jpg",
+			"4.jpg",
+			"5.jpg",
+			"6.jpg",
+			"7.jpg",
+			"8.jpg"
+		],
+		"descripcion": "La casa club se ubica en el corazón de las parcelas  de carácter recreativo en las faldas del volcán de Santa Ana, en las cuales el usuario disfruta de la vegetación y clima del bosque nebuloso.<br/ >Su arquitectura manifiesta un carácter moderno mezclado con detalles propios de las casas patronales de las fincas de café de la zona. Su principal función es la de reunir ahí a los vecinos atraídos por el área de restaurante y tienda de conveniencia."
+	},
+	{
+		"name": "Casa Bosques De Altamira",
+		"ubicacion": "Colonia Altamira, Antiguo Cuscatlán San Salvador, El Salvador",
+		"area": "450.00 m²",
+		"arquitectura": "Arq. Ricardo González Zeceña",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Mario Reni Roldan",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "07",
+		"imagesFiles": [
+			"1.jpg",
+			"2.jpg",
+			"3.jpg"
+		],
+		"descripcion": "La planta arquitectónica se desarrolla en varias terrazas debido a las características propias del terreno. Se buscó que la diferencia de niveles entre las distintas áreas de la vivienda, no causaran impacto, tanto visual como físico al recorrerlas, pero si aprovechar para generar distintas alturas en los volúmenes de sus fachadas dándole un aspecto muy dinámico al conjunto."
+	},
+	{
+		"name": "Casa Montebello",
+		"ubicacion": "Santa Catarina Pínula, Guatemala, Guatemala",
+		"area": "500.00 m²",
+		"arquitectura": "Grupo AG Constructores S.A.",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Alejandro González",
+		"jardinizacion": "",
+		"ingenieria": "Ing. Eduardo Aballi",
+		"montaje": "",
+		"imagesFolder": "08",
+		"imagesFiles": [
+			"DSCN6174.jpg",
+			"DSCN6180.JPG",
+			"IMG_1379.JPG",
+			"IMG_1380.JPG",
+			"IMG_3534.JPG",
+			"IMG_3823.JPG",
+			"IMG_3856.JPG",
+			"IMG_3866.JPG",
+			"IMG_7791.jpg",
+			"IMG_7894.JPG",
+			"IMG_8452.JPG",
+			"IMG_8457.JPG",
+			"IMG_8462.JPG",
+			"IMG_8465.JPG",
+			"IMG_8468.JPG",
+			"IMG_8471.JPG"
+		],
+		"descripcion": "Este proyecto se desarrolla en un área montañosa en la periferia de la ciudad de Guatemala, lo que lo convierte en algo muy especial por las características propias del terreno, su entorno natural así como las majestuosas vistas hacia la ciudad y los principales volcanes de la cordillera. (Volcán de Agua, Acatenango y Fuego).<br/>Su arquitectura moderna logra integrarse dócilmente a la topografía quebrada del terreno, logrando suavizar la diferencia entre los distintos niveles de sus terrazas, invitando a recorrer fácilmente cada nivel.<br/>Dadas las características del entorno se buscó impactar a los usuarios con la vista abierta y franca hacia los volcanes y la ciudad, desde algunas áreas como el vestíbulo exterior de entrada y sus terrazas exteriores hacia el jardín y piscina."
+	},
+	{
+		"name": "Montecancinos S.A.",
+		"ubicacion": "Puerto San José, Escuintla, Guatemala",
+		"area": "470.00 m²",
+		"arquitectura": "Grupo AG Constructores S.A",
+		"construccion": "",
+		"fotografia": "Alejandro González",
+		"jardinizacion": "Grupo AG Constructores S.A.",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "09",
+		"imagesFiles": [
+			"101-1101_IMG.png",
+			"109-0914_IMG.JPG",
+			"111-1108_IMG.JPG",
+			"111-1136_IMG.JPG",
+			"111-1138_IMG.JPG"
+		],
+		"descripcion": "El proyecto  se desarrolla al interior del Residencial Las Olas, próximo a la costa sur en el océano Pacifico de Guatemala, con la finalidad de albergar a grupos internacionales de pesca deportiva.<br/>El diseño arquitectónico busca integrar el sabor tropical con la arquitectura colonial propia del país.<br/>El proyecto esta concebido en un solo módulo en el cual se distribuyen seis apartamentos privados e independientes que comparten las áreas de esparcimiento como el bar, la cocina, comedor y piscina."
+	},
+	{
+		"name": "San Miguel La Palotada",
+		"ubicacion": "Municipio de San Andrés, Petén, Guatemala",
+		"area": "1200.00 m²",
+		"arquitectura": "Grupo AG Constructores S.A.",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Alejandro González",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "12",
+		"imagesFiles": [
+			"P1070961.JPG",
+			"IMG_1309.JPG",
+			"IMG_3496.JPG",
+			"IMG_8042.JPG",
+			"IMG_8043.JPG",
+			"IMG_8052.JPG",
+			"IMG_8057.JPG",
+			"IMG_8074.JPG",
+			"IMG_8087.JPG",
+			"IMG_8096.JPG",
+			"IMG_8107.JPG",
+			"IMG_8122.JPG",
+			"IMG_8131.JPG",
+			"IMG_8132.JPG",
+			"IMG_8135.JPG",
+			"IMG_8138.JPG",
+			"IMG_8151.JPG",
+			"IMG_8175.JPG"
+		],
+		"descripcion": "El proyecto ¨Centro de Operaciones Conjuntas San Miguel La Palotada¨ de carácter institucional  cuya finalidad es albergar a las distintas entidades encargadas de la conservación de la Biósfera Maya, se encuentra ubicada en el área norte de la selva del Petén.<br>El Edificio consigue integrarse al  entorno natural y topografía del terreno."
+	},
+	{
+		"name": "Bodega De Usos Multiples",
+		"ubicacion": "Colonia San Benito, San Salvador",
+		"area": "300.00 m²",
+		"arquitectura": "",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "05",
+		"imagesFiles": [
+			"1.jpg",
+			"2.jpg",
+			"3.jpg",
+			"4.jpg",
+			"5.jpg"
+		],
+		"descripcion": "El diseño se conforma de estructura metálica espacial para cubrir adecuadamente las luces entre apoyos. Habiendo fabricado cada elemento (vigas y columnas) en los talleres de la empresa."
+	},
+	{
+		"name": "Piscina",
+		"ubicacion": "Residencial Las Luces, Santa Catarina Pínula, Guatemala",
+		"area": "40 m²",
+		"arquitectura": "",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Alejandro González",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "10",
+		"imagesFiles": [
+			"IMG_1703.JPG",
+			"IMG_1704.JPG",
+			"IMG_1705.JPG",
+			"IMG_1706.JPG",
+			"IMG_1707.JPG",
+			"IMG_1708.JPG",
+			"IMG_1709.JPG",
+			"IMG_1710.JPG",
+			"IMG_1711.JPG",
+			"IMG_4789.JPG",
+			"IMG_5237.jpg"
+		],
+		"descripcion": "Estructura monolítica de concreto armado."
+	},
+	{
+		"name": "Edificio Administrativo Y Sala De Ventas Helados POPS",
+		"ubicacion": "Avenida de Las Américas, Zona 13, Guatemala, Guatemala",
+		"area": "1500.00 m²",
+		"arquitectura": "Grupo AG Constructores S.A.",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Alejandro González",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "11",
+		"imagesFiles": [
+			"IMG_9896.JPG",
+			"IMG_9897.JPG",
+			"IMG_9904.JPG",
+			"IMG_9906.JPG",
+			"IMG_9907.JPG",
+			"IMG_9908.JPG",
+			"IMG_9909.JPG",
+			"IMG_9910.JPG",
+			"IMG_9911.JPG"
+		],
+		"descripcion": "El edifico y la tienda principal de la marca de helados se encuentra ubicado al costado oeste de la Avenida de Las Américas, avenida importante dentro del casco urbano de la ciudad.<br/>En el Edificio se encuentran las oficinas administrativas y cuartos fríos para el almacenaje de producto terminado. "
+	},
+	{
+		"name": "SEASIDE",
+		"ubicacion": "Residencial Seaside, Puerto San José, Escuintla, Guatemala",
+		"area": "320.00 m²",
+		"arquitectura": "Grupo AG Constructores S.A.",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Alejandro González",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "13",
+		"imagesFiles": [
+			"IMG_2434.JPG",
+			"IMG_2438.JPG",
+			"IMG_2492.JPG",
+			"IMG_2523.JPG"
+			],
+		"descripcion": "La casa se encuentra rodeada de áreas verdes y su propio jardín en donde la piscina forma parte integral de su arquitectura por la forma en que se interrelaciona con las áreas de sala y los dormitorios de la planta baja. Su arquitectura es de líneas simples y rectas adornada con estructuras de madera en forma de pérgolas para enmarcar las paredes de mampostería y generar la sombra necesaria para el comfort térmico de las áreas interiores."
+	},
+	{
+		"name": "Rampa Fenix (Bodega en Puerto Quetzal)",
+		"ubicacion": "Empresa Portuaria Quetzal, Puerto San José, Escuintla, Guatemala",
+		"area": "800.00 m²",
+		"arquitectura": "",
+		"construccion": "",
+		"fotografia": "Alejandro González",
+		"jardinizacion": "",
+		"ingenieria": "Grupo AG Constructores S.A.",
+		"montaje": "Grupo AG Constructores S.A.",
+		"imagesFolder": "15",
+		"imagesFiles": ["BodegasPuertoQuetzal01.jpg","BodegasPuertoQuetzal02.jpg","BodegasPuertoQuetzal03.JPG","BodegasPuertoQuetzal06.JPG","BodegasPuertoQuetzal11.JPG","BodegasPuertoQuetzal17.JPG"],
+		"descripcion": "El proyecto de carácter industrial contempló el cálculo, fabricación y montaje de la estructura metálica en un muelle para descarga y revisión de contenedores, previo a su ingreso al recinto fiscal de la Empresa Portuaria Quetzal.<br/>Esta obra de estructura metálica forma parte del taller de la empresa, dirigida a la industria en cuestión de diseño, fabricación y montaje de estructuras."
+	},
+	{
+		"name": "Casa Finca San Antonio",
+		"ubicacion": "Finca San Antonio, San José Pínula, Guatemala.",
+		"area": "350.00 m²",
+		"arquitectura": "Arq. Luis Enrique Zeceña",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Arq. Luis Enrique Zeceña",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "14",
+		"imagesFiles": ["lafoto1.JPG","lafoto2.JPG","lafoto3.JPG","lafoto4.JPG"],
+		"descripcion": "El proyecto se desarrolla en un terreno plano, lo que fuera el área de jardines del casco original de la Finca San Antonio.<br/>La casa se distribuye en una sola planta para integrase finamente al paisaje y jardines debiendo de mantener un carácter campestre pero a su vez contemporáneo.  Las cenefas de mampostería y techos inclinados, responden a la  necesidad de recolectar el agua de lluvia durante la época  tiempo de invierno de una manera discreta."
+	},
+	{
+		"name": "Don Emiliano",
+		"ubicacion": "Centro Comercial Oakland Mall, zona 10, Gutemala",
+		"area": "145.00 m²",
+		"arquitectura": "Arq. Enrique Zeceña",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Alejandro González",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "16",
+		"imagesFiles": ["IMG_8887.JPG","IMG_8888.JPG","IMG_8890.JPG","IMG_8891.JPG","IMG_8893.JPG","IMG_8897.JPG"],
+		"descripcion": ""
+	},
+
+	{
+		"name": "HERR SALCHICHA",
+		"ubicacion": "Centro Comercial El Campanario, zona 10, Guatemala",
+		"area": "85 m²",
+		"arquitectura": "Arq. Alejandro González",
+		"construccion": "Grupo AG Constructores S.A.",
+		"fotografia": "Alejandro González",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "19",
+		"imagesFiles": ["IMG_7609.jpg","IMG_7607.jpg","IMG_7608.jpg","IMG_7612.jpg","IMG_7616.JPG","IMG_7617.JPG","IMG_7618.jpg"],
+		"descripcion": ""
+	},
+	{
+		"name": "INAP",
+		"ubicacion": "",
+		"area": "m²",
+		"arquitectura": "",
+		"construccion": "",
+		"fotografia": "",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "17",
+		"imagesFiles": ["1.jpg","2.jpg"],
+		"descripcion": ""
+	},
+	{
+		"name": "Yaxhá",
+		"ubicacion": "",
+		"area": "m²",
+		"arquitectura": "",
+		"construccion": "",
+		"fotografia": "",
+		"jardinizacion": "",
+		"ingenieria": "",
+		"montaje": "",
+		"imagesFolder": "18",
+		"imagesFiles": ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg"],
+		"descripcion": ""
+	}
+]
+
+
+var pname = function(i){
+	return "<h2 class='text-center'>"+agc[i].name+"</h2>";
+};
+var pubicacion = function(i){
+	if (agc[i].ubicacion != "") {
+		return "<p><b class='text-center'>Ubicación:</b> "+agc[i].ubicacion+"</p>";
+	}else{
+		return "";
+	}
+};
+var parea = function(i){
+	if (agc[i].area != "m²") {
+		return "<p><b>Área: </b>"+agc[i].area+"</p>";
+	}else{
+		return "";
+	}
+};
+var parquitectura = function(i){
+	if (agc[i].arquitectura != "") {
+		return "<p><b>Arquitectura: </b>"+agc[i].arquitectura+"</p>";
+	}else{
+		return "";
+	}
+};
+var pconstruccion = function (i) {
+	if (agc[i].construccion != "") {
+		return "<p><b>Construcción: </b>"+agc[i].construccion+"</p>";
+	}else{
+		return "";
+	}	
+};
+var pjardinizacion = function (i) {
+	if (agc[i].jardinizacion != "") {
+		return "<p><b>Jardinización: </b>"+agc[i].jardinizacion+"</p>";
+	}else{
+		return "";
+	}
+};
+var pfotografia = function (i) {
+	if (agc[i].fotografia != "") {
+		return "<p><b>Fotografía: </b>"+agc[i].fotografia+"</p>";
+	}else{
+		return "";
+	}
+};
+var pimage = function(i){
+	return '<img src="img/'+agc[i].imagesFolder+'/'+agc[i].imagesFiles[1]+'" class="img-responsive">'
+};
+var pingenieria = function(i){
+	if (agc[i].ingenieria != "") {
+		return "<p><b>Ingenieria: </b>"+agc[i].ingenieria+"</p>";
+	}else{
+		return "";
+	}
+};
+var pmontaje = function(i){
+	if (agc[i].montaje != "") {
+		return "<p><b>Montaje: </b>"+agc[i].montaje+"</p>";
+	}else{
+		return "";
+	}
+};
+var pdescripcion = function(i){
+	if (agc[i].descripcion != "") {
+		return "<p class='text-justify'><b>Descripción: </b>"+agc[i].descripcion+"</p>";
+	}else{
+		return "";
+	}
+};
+
+for (var i= 0; i <= agc.length; i+=3) {
+$("#projects").append('<div class="row" id="projects'+i+'"></div>')
+$("#projects"+i).append('<div class="col-xs-12 col-md-4 well">'+pname(i)+pimage(i)+'<br>'+pubicacion(i)+parea(i)+parquitectura(i)+pconstruccion(i)+pingenieria(i)+pjardinizacion(i)+pmontaje(i)+pfotografia(i)+pdescripcion(i)+'</div>');
+$("#projects"+i).append('<div class="col-xs-12 col-md-4 well">'+pname(i+1)+pimage(i+1)+'<br>'+pubicacion(i+1)+parea(i+1)+parquitectura(i+1)+pconstruccion(i+1)+pingenieria(i+1)+pjardinizacion(i+1)+pmontaje(i+1)+pfotografia(i+1)+pdescripcion(i+1)+'</div>');
+$("#projects"+i).append('<div class="col-xs-12 col-md-4 well">'+pname(i+2)+pimage(i+2)+'<br>'+pubicacion(i+2)+parea(i+2)+parquitectura(i+2)+pconstruccion(i+2)+pingenieria(i+2)+pjardinizacion(i+2)+pmontaje(i+2)+pfotografia(i+2)+pdescripcion(i+2)+'</div>');
+}
